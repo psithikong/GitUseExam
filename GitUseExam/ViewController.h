0ca3208695d012325a4895d2cdae0a07a4d3ddfa
paeng <paeng@wl-10-190-211-232.wireless.duke.edu>
2013-09-17 16:11:12 -0400

    Initial Commit

---
+++ GitUseExam/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExam
+//
+//  Created by paeng on 9/17/13.
+//  Copyright (c) 2013 paeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUseExam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitUseExam
+//
+//  Created by paeng on 9/17/13.
+//  Copyright (c) 2013 paeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
